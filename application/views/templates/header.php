0561d893766fbd996719b9c8e7899761a4a84233
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2018-11-14 20:42:13 +0700

    add navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -11,3 +11,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script src="<?=base_url('assets/js/jquery-3.2.1.min.js');?>"></script>
     <script src="<?=base_url('assets/js/bootstrap.min.js');?>"></script>
 </head>
+
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<b><a class="navbar-brand" href="https://github.com/Cvar1984/bacod">Bacod</a></b>
+		</div>
+
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="<?=base_url('index.php');?>">Home</a></li>
+				<li><a href="<?=base_url('index.php/chat');?>">Chat</a></li>
+				<li><a href="<?=base_url('index.php/record');?>">Record</a></li>
+			</ul>
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="<?=base_url('index.php/register');?>">Register</a></li>
+				<li><a href="<?=base_url('index.php/login');?>">Login</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li><a href="#">Separated link</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div>
+</nav>
